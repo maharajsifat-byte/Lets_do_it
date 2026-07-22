2457b3243caf4b1d037b93db3b9c2d8dd278200a
Maharaj Sifat <maharajsifat@gmail.com>
2026-07-22 20:08:44 +0600

    Update stats_engine.py

--- stats_engine.py
+++ stats_engine.py
@@ -1,4 +1,16 @@
 import numpy as np
 class StatsEngine:
     @staticmethod
-    
+    def calculate_metrics(results):
+        if not results: return None
+        scores = np.array([r['score'] for r in results])
+        
+        return {
+            "Total Attempts": len(scores),
+            "Average Score": np.mean(scores),
+            "Max Score": np.max(scores),
+            "Min Score": np.min(scores),
+            "Median Score": np.median(scores),
+            "Standard Deviation": np.std(scores),
+            "Pass Percentage": (np.sum(scores >= 2) / len(scores)) * 100 
+        }    
