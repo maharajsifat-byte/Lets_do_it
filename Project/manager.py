27f4c872bad526d6a3fcdf6d37bd9a36b9c32588
Maharaj Sifat <maharajsifat@gmail.com>
2026-07-22 19:48:42 +0600

    Done

--- Project/manager.py
+++ Project/manager.py
@@ -1,5 +1,12 @@
+from typing import Any, cast
+
 from database import Database
 
 class QuestionManager:
     def __init__(self):
         self.db = Database()
+    def delete_question(self, q_id):
+        db = cast(Any, self.db)
+        qs = db.load_data(db.q_file)
+        qs = [q for q in qs if q['id'] != q_id]
+        db.save_data(db.q_file, qs)
