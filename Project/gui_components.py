9848b3bd2d3992f4493a1aa543ae95c76145a962
Maharaj Sifat <maharajsifat@gmail.com>
2026-07-23 20:17:44 +0600

    Add Custom Button Class
    class StyledButton inherited .def __init__(self, parent, text, command, **kwargs) it will creeate  parent , text command the constructor function

--- Project/gui_components.py
+++ Project/gui_components.py
@@ -1,3 +1,11 @@
 import tkinter as tk 
 from tkinter import ttk , messagebox
 from styles import *
+class StyledButton(tk.Button):
+    def __init__(self, parent, text, command, **kwargs):
+        super().__init__(
+            parent, text=text, command=command,
+            bg=ACCENT_COLOR, fg=BG_DARK,
+            activebackground=C_HOVER_COLOR, activeforeground=BG_DARK,
+            font=FONT_REG, bd=0, cursor="hand2", padx=15, pady=8, **kwargs
+        )
