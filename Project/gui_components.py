d88bea2da70c3c03f81e864e624c49f84a06fefa
Maharaj Sifat <maharajsifat@gmail.com>
2026-07-23 20:30:25 +0600

    Pop-up Modal Dialog
    Window pop-up . New pop-up window . Execute class . Full fix the pop-up window .

--- Project/gui_components.py
+++ Project/gui_components.py
@@ -1,5 +1,6 @@
 import tkinter as tk 
 from tkinter import ttk , messagebox
+from turtle import title
 from styles import *
 class StyledButton(tk.Button):
     def __init__(self, parent, text, command, **kwargs):
@@ -12,3 +13,13 @@ class StyledButton(tk.Button):
         class StyledEntry(ttk.Entry):
             def __init__(self, parent, **kwargs):
               super().__init__(parent, font=FONT_REG, **kwargs)
+
+        class QuestionFormDialog(tk.Toplevel):
+           def __init__(self, parent, title, on_save, initial_data=None):
+             super().__init__(parent)
+             self.title(title)
+             self.geometry("500x550")
+             self.configure(bg=BG_DARK)
+             self.on_save = on_save
+             self.initial_data = initial_data
+             self.setup_ui()
