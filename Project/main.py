6d7f8e6248637c4fbbb21b05e277ca3bdc1d885d
Tozo <25-61465-1@student.aiub.edu>
2026-07-24 02:18:42 +0600

    Defined main class and constructor for it
    in the constructor did some layout and UI framework

--- Project/main.py
+++ Project/main.py
@@ -7,3 +7,21 @@ from database import Database
 from manager import Manager
 from stats_engine import StatsEngine
 from pdf_parser import PDFparser 
+from gui_components import StyledButton, QuestionFormDialog
+from styles import *
+
+class MainApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Varsity Quiz System Pro")
+        self.root.geometry("1150x780")
+        self.root.configure(bg=BG_DARK)
+
+        self.db = Database()
+        self.manager = QuestionManager()
+        self.current_user = None
+
+        self. main_container = tk.Frame(self.root, bg= BG_DARK)
+        self.main_container.pack(fill="both", expand=True)
+
+        self.show_auth_screen()
