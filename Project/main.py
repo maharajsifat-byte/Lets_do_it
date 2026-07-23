55e8e10827b08acd97e92bf92804eb2af1b4ce90
Tozo <25-61465-1@student.aiub.edu>
2026-07-24 01:54:05 +0600

    Imported tkinter, datetime, os
    imported packages tkinter for gui
    imported datetime for time tracking
    imported os to interect with pc's file

---
+++ Project/main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox, filedialog, ttk
+from datetime import datetime 
+import os
+
+from database import Database
+from manager import Manager
+from stats_engine import StatsEngine
+from pdf_parser import PDFparser 
