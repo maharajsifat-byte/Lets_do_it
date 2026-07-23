d6d2aa43c5982f18e49f49d84d6939cb9d2d469a
Tozo <25-61465-1@student.aiub.edu>
2026-07-24 02:57:44 +0600

    Initializing main MainApp core structure and dynamic auth UI
    built dynamic authentication frame for login and registration.
    
    added initial GUI and login screen.
    
    initialized manager and database classes.

--- Project/main.py
+++ Project/main.py
@@ -25,3 +25,27 @@ class MainApp:
         self.main_container.pack(fill="both", expand=True)
 
         self.show_auth_screen()
+
+    def clear_screen(self):
+        for w in self.main_container.winfo_children():
+            w.destroy()
+
+    def show_auth_screen(self,mode="login"):
+        self.clear_screen()
+
+        card = tk.Frame(self.main_container, bg=SIDEBAR_COLOR, padx=40, pady=40)
+        card.place(relx=0.5, rely=0.5, anchor="center")
+
+        title_text = "STUDENT LOGIN" if mode == "login" else "CREATE ACCOUNT"
+        tk.Label(card, text=title_text, font=("Seoge UI", 22,"bold"), bg=SIDEBAR_COLOR, fg=ACCENT_COLOR).pack(pady=(10, 5))
+
+        subtitle = "Access quix engine & analytics" if mode== "login" else "Join the academic quiz dashboard"
+        tk.Label(card, text=subtitle, font=("Seoge UI", 10), bg=SIDEBAR_COLOR, fg="grey").pack(pady=(0,25))
+
+        tk.Label(card, text="Username", font=("Seoge UI", 10, "bold"), bg=SIDEBAR_COLOR, fg=TEXT_WHITE).pack(anchor="W", pady=(5,2))
+        u_ent = ttk.Entry(card, width=35, font=("Seoge UI", 11))
+        u_ent.pack(pady=(0,15))
+
+        tk.Label(card, text="Password", font=("Seoge UI", 10, "bold"), bg=SIDEBAR_COLOR, fg=TEXT_WHITE).pack(anchor="w", pady=(5,2))
+        p_ent = ttk.Entry(card, width=35, show="*", font=("Seoge UI", 11))
+        p_ent.pack(pady=(0,15))
