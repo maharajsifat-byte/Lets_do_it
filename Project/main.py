f54b2b2ec401696b4295578faf15f5a75b396e65
abid <25-61471-1@student.aiub.edu>
2026-07-25 15:37:01 +0600

    fixing tozos mistake

--- Project/main.py
+++ Project/main.py
@@ -2,11 +2,11 @@ import tkinter as tk
 from tkinter import messagebox, filedialog, ttk
 from datetime import datetime 
 import os
-
+import json
 from database import Database
 from manager import QuestionManager
 from stats_engine import StatsEngine
-from pdf_parser import PDFparser 
+from pdf_parser import PDFProcessor
 from gui_components import StyledButton, QuestionFormDialog
 from styles import *
 
